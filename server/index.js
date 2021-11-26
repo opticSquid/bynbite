995aa1e249fc22f3a3335c16657e5498f467720a
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2021-11-27 00:14:15 +0530

    removing cors in production

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
