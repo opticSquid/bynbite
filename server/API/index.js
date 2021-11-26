53c3700503cab335107897ac49feff1a8fda16b2
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2021-11-26 23:44:51 +0530

    adding call back url to regenerate access token.

--- server/API/index.js
+++ server/API/index.js
@@ -4,4 +4,6 @@ router.get("/", (req, res) => {
   res.status(200).json({ status: "API working correctly!" });
 });
 router.use("/auth", require("./auth"));
+// Use this route to regenerate the access token. Given the refresh token is provided in authorization header.
+router.use("/token", require("./regenerate"));
 module.exports = router;
